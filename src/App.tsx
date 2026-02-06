ee23a6618964116ce90171ddf76130cf5678ca90
Doha-brd <dohaalaoui408@gmail.com>
2026-02-06 15:56:46 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import { Experience } from "./components/Experience";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import { HashRouter } from 'react-router-dom'
 
+// ...
+<HashRouter>
+  <App />
+</HashRouter>
 export function App() {
   return (
     <div className="min-h-screen text-slate-200 selection:bg-cyan-500/30 selection:text-cyan-200">
